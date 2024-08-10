30e3bd4c62414c852210181b9c4e1da51c63dc33
Frannbau <francorovelazquez@gmail.com>
2024-08-10 17:28:21 -0300

    Agrego clase_6.py

---
+++ clase_6.py
@@ -0,0 +1,6 @@
+"""Cree un programa que pida y muestre 10 numeros.
+Luego guardarlo en un repositorio local para luego subirlo a un repositorio remoto en github"""
+
+for i in range (10):
+    num=input("Ingrese un numero:")
+    print(num)
